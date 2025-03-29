77aab584fce7d32cee4716fca0000be5c179fde8
Hudson Schumaker <hudson.schumaker@me.com>
2025-03-29 14:32:31 +0100

    remove warnings

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
+#include "include/pch.h"
 #include "include/de_gfx.h"
-#include "include/de_program.h"
+#include "include/de_scene.h"
+#include "playground/title_screen.h"
+#include "playground/splash_screen.h"
 
-int main(int argc, const char* argv[]) {
-	
-	// Initialize SDL
+int main(int argc, char* argv[]) {	
 	gfx_init(false, true);
-	program_t* program = program_new();
+	
 
-	program_compile(program, SHADER_FOLDER"basic.vert", SHADER_FOLDER"basic.frag");
+	splash_screen_init();
+	title_screen_init();
 
+	scene_t* splash_screen = splash_screen_get_scene();
+	scene_t* title_screen = title_screen_get_scene();
+
+	short r = scene_manager_set_scene(splash_screen);
+	printf("Scene loop returned %d\n", r);
+	r = scene_manager_set_scene(title_screen);
+	printf("Scene loop returned %d\n", r);
 
 	return 0;
 }
