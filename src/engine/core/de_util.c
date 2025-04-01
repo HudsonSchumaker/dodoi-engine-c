338b82014396f832a9182a994efd344dd3feab69
Hudson Schumaker <hudson.schumaker@me.com>
2025-04-01 14:16:16 +0200

    update util.

--- src/engine/core/de_util.c
+++ src/engine/core/de_util.c
@@ -12,59 +12,6 @@ int sign(register float value) {
 	return (value > 0) - (value < 0);
 }
 
-float minf(register float a, register float b) {
-    return (a < b) ? a : b;
-}
-
-float maxf(register float a, register float b) {
-    return (a > b) ? a : b;
-}
-
-float clampf(register float value, register float min_val, register float max_val) {
-    return maxf(min_val, minf(value, max_val));
-}
-
-float interpolatef(register float start, register float end, register float t) {
-    return start + t * (end - start);
-}
-
-float deg_to_rad(register float degrees) {
-    return degrees * DIV_PI_180;
-}
-
-float rad_to_deg(register float radians) {
-    return radians * DIV_180_PI;
-}
-
-float smoothstep(register float edge0, register float edge1, register float x) {
-    x = clampf((x - edge0) / (edge1 - edge0), 0.0f, 1.0f);
-    return x * x * (3 - 2 * x);
-}
-
-float inverse_lerp(register float a, register float b, register float value) {
-    return (value - a) / (b - a);
-}
-
-float wrap(register float value, register float min_val, register float max_val) {
-    register float range = max_val - min_val;
-    return min_val + fmodf(fmodf(value - min_val, range) + range, range);
-}
-
-double clamp(register double value, register double min_val, register double max_val) {
-    return max(min_val, min(value, max_val));
-}
-
-double interpolate(register double start, register double end, register double t) {
-    return start + t * (end - start);
-}
-
-#pragma intrinsic(sqrtf)
-float distance_between_points(const float x1, const float y1, const float x2, const float y2) {
-    // using the euclidean distance formula
-    return sqrtf((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-}
-
-
 char* concat(const char* s1, const char* s2) {
     const size_t len1 = strlen(s1);
     const size_t len2 = strlen(s2);
--- src/include/de_util.h
+++ src/include/de_util.h
@@ -9,21 +9,5 @@
 #pragma once
 #include "pch.h"
 
-char* concat(const char* s1, const char* s2);
-
 int sign(float value);
-
-float minf(float a, float b);
-float maxf(float a, float b);
-float wrap(float value, float min_val, float max_val);
-float clampf(float value, float min_val, float max_val);
-float deg_to_rad(float degrees);
-float rad_to_deg(float radians);
-float smoothstep(float edge0, float edge1, float x);
-float inverse_lerp(float a, float b, float value);
-float interpolatef(float start, float end, float t);
-
-double clamp(double value, double min_val, double max_val);
-double interpolate(double start, double end, double t);
-
-float distance_between_points(const float x1, const float y1, const float x2, const float y2);
+char* concat(const char* s1, const char* s2);
