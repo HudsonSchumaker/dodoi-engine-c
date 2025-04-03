f3d0992ae4d4f082fae89af453ae7905aee1dd89
Hudson Schumaker <hudson.schumaker@me.com>
2025-04-03 19:05:21 +0200

    update cube header

--- src/include/de_cube.h
+++ src/include/de_cube.h
@@ -5,7 +5,7 @@ typedef struct {
 	game_object_t go;
 } cube_t;
 
-void cube_init(cube_t* cube, const char* vertex_shader, const char* fragment_shader, const char* texture);
+void cube_init(cube_t* cube, const char* vertex_shader, const char* fragment_shader, const char* texture, const char* model);
 void cube_render(cube_t* cube, mat4_t* view, mat4_t* projection);
 void cube_update(cube_t* cube);
 void cube_delete(cube_t* cube);
