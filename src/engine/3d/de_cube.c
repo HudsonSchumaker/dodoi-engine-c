8cdd900ee43890566629e503207d4867c436c2ec
Hudson Schumaker <hudson.schumaker@me.com>
2025-04-14 21:01:29 +0200

    update cube.

--- src/engine/3d/de_cube.c
+++ src/engine/3d/de_cube.c
@@ -12,24 +12,6 @@ void cube_init(cube_t* cube, const char* vertex_shader, const char* fragment_sha
 	game_object_3d_init(&cube->go, vertex_shader, fragment_shader, texture, model);
 
     vao_bind(&cube->go.vao);
-	
-	float* vertex_buffer = mesh_vertex_to_gl_buffer(&cube->go.mesh);
-	for (int i = 0; i < cube->go.mesh.vertex_count; i++) {
-		printf("Vertex %d: Position(%f, %f, %f), Normal(%f, %f, %f), UV(%f, %f)\n",
-			i,
-			vertex_buffer[i * 8 + 0], vertex_buffer[i * 8 + 1], vertex_buffer[i * 8 + 2],
-			vertex_buffer[i * 8 + 3], vertex_buffer[i * 8 + 4], vertex_buffer[i * 8 + 5],
-			vertex_buffer[i * 8 + 6], vertex_buffer[i * 8 + 7]);
-	}
-	
-	unsigned int* index_buffer = mesh_index_to_gl_buffer(&cube->go.mesh);
-	for (int i = 0; i < cube->go.mesh.face_count; i++) {
-		printf("Face %d: Indices(%u, %u, %u)\n",
-			i,
-			index_buffer[i * 3 + 0], index_buffer[i * 3 + 1], index_buffer[i * 3 + 2]);
-	}
-
-
 	vbo_set_data(&cube->go.vbo, mesh_vertex_to_gl_buffer(&cube->go.mesh), cube->go.mesh.vertex_count * 8 * sizeof(float));
 	ebo_set_data(&cube->go.ebo, mesh_index_to_gl_buffer(&cube->go.mesh), cube->go.mesh.face_count * 3 * sizeof(unsigned int));
 	
@@ -47,7 +29,6 @@ void cube_render(cube_t* cube, mat4_t* view, mat4_t* projection) {
 	program_set_uniform_mat4f(cube->go.uniform_view, view);
 	program_set_uniform_mat4f(cube->go.uniform_projection, projection);
 
-	//glDrawElements(GL_TRIANGLES, cube->go.mesh.face_count * 3, GL_UNSIGNED_INT, 0);
 	glDrawArrays(GL_TRIANGLES, 0, cube->go.mesh.vertex_count);
 
 	buffer_unbind();
