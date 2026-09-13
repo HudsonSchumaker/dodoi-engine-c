c34e8dfd06481331a2d4abf4e948abc184f8b7cb
Schumaker <hudson.schumaker@me.com>
2026-09-13 11:53:49 +0200

    Dodoi-Lab: add fast sqrt.

--- src/include/de_math.h
+++ src/include/de_math.h
@@ -13,3 +13,25 @@ float smoothstepf(float edge0, float edge1, float x);
 float inverse_lerpf(float a, float b, float value);
 float interpolatef(float start, float end, float t);
 float distance_between_points(const float x1, const float y1, const float x2, const float y2);
+
+static inline float rsqrtf(float number) {
+    union {
+        float f;
+        int i;
+    } conv;
+    float x2, y;
+    const float threehalfs = 1.5f;
+
+    x2 = number * 0.5f;
+    conv.f = number;
+
+    conv.i = 0x5f3759df - (conv.i >> 1);
+    y = conv.f;
+
+    y = y * (threehalfs - (x2 * y * y));
+    return y;
+}
+
+static inline float fsqrtf(float number) {
+    return number * rsqrtf(number);
+}
